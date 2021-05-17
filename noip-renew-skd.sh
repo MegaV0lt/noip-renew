8731c9ee9007af2d8eb1e768115c2eaa9582ca77
MegaV0lt <MegaV0lt@users.noreply.github.com>
2021-05-17 16:33:34 +0200

    Minor changes

--- noip-renew-skd.sh
+++ noip-renew-skd.sh
@@ -8,11 +8,11 @@ INSTEXE="${INSTDIR}/noip-renew-${USER}.sh"
 #NEWCJOB="30 0 $1 $2 *   $USER    $INSTEXE $LOGDIR"
 CRONJOB="30 0  * * *  $INSTEXE $LOGDIR"
 NEWCJOB="30 0  $1 $2 *  $INSTEXE $LOGDIR"
-$SUDO crontab -u $USER -l | grep -v '/noip-renew*'  | $SUDO crontab -u $USER -
-if [[ $3 = "True" ]]; then
-    ($SUDO crontab -u $USER -l; echo "$NEWCJOB") | $SUDO crontab -u $USER -
+$SUDO crontab -u $USER -l | grep -v '/noip-renew*' | $SUDO crontab -u $USER -
+if [[ $3 == 'True' ]]; then
+  ($SUDO crontab -u $USER -l; echo "$NEWCJOB") | $SUDO crontab -u $USER -
 else
-    ($SUDO crontab -u $USER -l; echo "$CRONJOB") | $SUDO crontab -u $USER -
+  ($SUDO crontab -u $USER -l; echo "$CRONJOB") | $SUDO crontab -u $USER -
 fi
 
 exit 0
