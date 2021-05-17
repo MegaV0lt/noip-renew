6a1a6373d1a7d1dc19493ffa966f63f09ea67ffc
MegaV0lt <MegaV0lt@users.noreply.github.com>
2021-05-17 16:38:01 +0200

    Update noip-renew.sh
    Minor changes

--- noip-renew.sh
+++ noip-renew.sh
@@ -3,12 +3,12 @@
 USERNAME=""
 PASSWORD=""
 
-LOGDIR=$1
-PROGDIR=$(dirname -- $0)
+LOGDIR="$1"
+PROGDIR="$(dirname -- $0)"
 
-if [ -z "$LOGDIR" ]; then
-    $PROGDIR/noip-renew.py "$USERNAME" "$PASSWORD" 2
+if [[ -z "$LOGDIR" ]] ; then
+  ${PROGDIR}/noip-renew.py "$USERNAME" "$PASSWORD" 2
 else
-    cd $LOGDIR
-    $PROGDIR/noip-renew.py "$USERNAME" "$PASSWORD" 0 >> $USERNAME.log
+  cd $LOGDIR
+  ${PROGDIR}/noip-renew.py "$USERNAME" "$PASSWORD" 0 &>> "${USERNAME}.log"
 fi
